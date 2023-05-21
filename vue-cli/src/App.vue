c1379e03085bc7bae2bc48ab9d34dae0137c79ba
minji428 <alswl980428@gmail.com>
2023-05-21 14:00:13 +0900

    Vue.js 시작하기

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <app-header></app-header> -->
+    <!-- <hello-world></hello-world> -->
+    <HelloWorld msg="Welcome to Your Vue.js"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeaderVue from './components/AppHeader.vue'
+
+export default {
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  },
+  components: {
+    // 'app-header' : AppHeaderVue
+    HelloWorld,
+    'hello-world' : HelloWorld,
+  }
+}
+</script>
+
+<style>
+
+</style>
